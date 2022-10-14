6348f5e02c381ebd68f0f1a1ee288769e9a0f1ff
Lara Nettleton <lara.nettleton@kcl.ac.uk>
2022-10-14 14:12:38 +0100

    raise value error if value enteres<=0

--- primes.py
+++ primes.py
@@ -3,4 +3,29 @@
 
 def primes(number_of_primes):
     list = []
-    return list
+    if number_of_primes>0:
+        counter = 0
+        i=2
+        while counter<number_of_primes:
+            if isPrime(i):
+                list.append(i)
+                counter = counter+1
+            i = i+1
+        return list
+    else:
+        raise ValueError
+
+
+def isPrime(number):
+    isPrime = True
+    if isinstance(number, int):
+        i = 2
+        while i<(number/2)+1 and isPrime==True:
+            if number%i==0:
+                isPrime=False
+            i = i+1
+    else:
+        isPrime = False
+    return isPrime
+
+#test
